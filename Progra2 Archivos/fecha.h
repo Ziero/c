5df60bd3b2ce5febe25e92d3fbb9057efffcd2fe
Cris245 <cristians245@gmail.com>
2013-09-03 11:33:22 -0300

    Fechas, .h y .c

---
+++ Progra2 Archivos/fecha.h
@@ -0,0 +1,11 @@
+#ifndef FECHA_H_INCLUDED
+#define FECHA_H_INCLUDED
+int cantdiasmes(int,int);
+int esFechaValida(const*);
+int difDia(const*,const*);
+int mesMenor(int,int,int,int);
+
+
+
+
+#endif // FECHA_H_INCLUDED
